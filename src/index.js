9e000ce08d94bc490ad334119b293c4f3b48870e
BlackDragDroit <gabrielgeyrhofer@gmail.com>
2023-12-19 22:30:35 +0100

    Update package.json and index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import express from "express";
+import * as brain from "brainjs";
+// import * as ml5 from "ml5";
+import * as fs from "file-system";
+import * as d3 from "d3";
+
 const app = express();
 
-
 app.get("/", (req, res) => {
-  res.send("Choo Choo! Welcome to your Express app 🚅");
+  const nn = new brain.NeuralNetwork({ hiddenLayers: [3, 4] });
+
+  const trainingData = [
+    { input: { r: 255, g: 25, b: 255 }, output: { white: 1 } },
+    { input: { r: 255, g: 255, b: 25 }, output: { black: 1 } },
+    { input: { r: 255, g: 255, b: 255 }, output: { white: 1 } },
+    { input: { r: 25, g: 55, b: 255 }, output: { black: 1 } },
+    { input: { r: 25, g: 255, b: 25 }, output: { white: 1 } },
+  ];
+
+  nn.train(trainingData);
+
+  res.send(nn.run({ r: 25, g: 255, b: 205 }));
 });
 
 // app.get("/json", (req, res) => {
 //   res.json({ "Choo Choo": "Welcome to your Express app 🚅" });
 // });
-
 
 // app.post("/post", (req, res) => {
 //   res.json({ "Choo Choo": "Welcome to your Express app 🚅", body: req.body });
@@ -18,5 +33,5 @@ app.get("/", (req, res) => {
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Assistant listening on port ${port}`);
 });
